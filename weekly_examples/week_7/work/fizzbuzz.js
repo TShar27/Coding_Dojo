a6e9e55d496429d4b67dca2ec690cd8e1973c406
Timmy Shar <timshar27@gmail.com>
2022-02-26 15:59:44 -0800

    add fizzbuzz

--- weekly_examples/week_7/work/fizzbuzz.js
+++ weekly_examples/week_7/work/fizzbuzz.js
@@ -23,19 +23,19 @@
 
 
 for (let i=1; i<=100; i++) {
-     if (i%15 == 0) {
-     console.log("FizzBuzz");
-     }
+    if (i%15 == 0) {
+    console.log("FizzBuzz");
+    }
 
-     else if ((i%3) == 0) {
-        console.log("Fizz" );
-     }
+    else if ((i%3) == 0) {
+       console.log("Fizz" );
+    }
 
-     else if ((i%5) == 0) {
-        console.log("Buzz");
-     }
+    else if ((i%5) == 0) {
+       console.log("Buzz");
+    }
 
-     else {
-        console.log(i);
-     }
+    else {
+       console.log(i);
+    }
 }
