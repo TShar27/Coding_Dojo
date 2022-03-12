c3ad917a004b209a0e7ae9c5dd564c8dc47c4c95
Timmy Shar <timshar27@gmail.com>
2022-03-12 11:59:37 -0800

    edits

--- python/weekly_work/helloworld.py
+++ python/weekly_work/helloworld.py
@@ -3,15 +3,15 @@ print( "Hello Wolrd")
 
 # 2. print "Hello Noelle!" with the name in a variable
 name = "Noelle"
-print(name)
-print(name + ",")	# with a comma
-print(name + " +")	# with a +
+
+print("Hello",name)	# with a comma
+print("Hello" + name)	# with a +
 
 # 3. print "Hello 42!" with the number in a variable
 name = "42"
 
-print("Hello " + name)	# with a comma
-print( "Hello + " + name)	# with a +	-- this one should give us an error!
+print("Hello " , name)	# with a comma
+print( "Hello " + name)	# with a +	-- this one should give us an error!
 
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
