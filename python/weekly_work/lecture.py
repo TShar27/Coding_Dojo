b075f877fcd42965f29d7310b0541a2501357ebc
Timmy Shar <timshar27@gmail.com>
2022-03-15 23:11:53 -0700

    week_2 lecture

--- python/weekly_work/lecture.py
+++ python/weekly_work/lecture.py
@@ -33,34 +33,62 @@
 
 # Dictionaries
 
-sports_cars = {
-    'Chevrolet': 'Corvette', # 'Key':'Value'
-    'Nissan': 'GTR',
-    'Ford': 'GT'
+# sports_cars = {
+#     'Chevrolet': 'Corvette', # 'Key':'Value'
+#     'Nissan': 'GTR',
+#     'Ford': 'GT'
+# }
+
+# print(sports_cars)
+# print(sports_cars['Nissan'])
+# print(sports_cars['GTR']) # Cannot call 'Key'
+
+# # adding a value to dictionary
+# sports_cars['Toyota']="Supra"
+
+# print(sports_cars)
+
+# # for loops 
+# for car in sports_cars:
+#     print(car) # will give us keys 
+#     print(sports_cars[car]) # will give us values 
+
+# # .keys()
+# for car in sports_cars.keys():
+#     print(car)
+
+# # .values()
+# for car in sports_cars.values():
+#      print(car)
+
+# # .items() - Prints both key and value. Can name the variables anything you want 
+# for key,val in sports_cars.items():
+#     print(key,val) 
+
+# create a function that adds a genre and list of artists onto our dictionary def add_genre_and_artists(genre,artists)
+from pickle import APPEND
+
+
+artists = {
+    'hip-hop': ['eminem','jay-z','snoop-dog'],
+    'rap': ['50 cent','dr. dre','j-cole'],
+    'country':  ['tim mcgraw','jason aldean','eric church'],
 }
 
-print(sports_cars)
-print(sports_cars['Nissan'])
-print(sports_cars['GTR']) # Cannot call 'Key'
+# def add_genre_and_artist(genre,artist_list):
+#     artists[genre] = artist_list
+#     return artists
 
-# adding a value to dictionary
-sports_cars['Toyota']="Supra"
+# print(add_genre_and_artist("pop",['bruno mars','the weeknd','justin beiber']))
+# print(add_genre_and_artist("haus",['black loops','shar bros','saison']))       
 
-print(sports_cars)
+# create a function that adds to the correct genre
+def add_to_genre(genre, artist_list):
+    if genre in artists:
+        # artists[genre].append(artist_list) # if we didnt use the for loop, the values in the fuction would still be added to the dictionary but they would be added outside of the country list, It would create a separate list added to the end of all the values
+        for artist in artist_list:
+            print(artist)
+            artists[genre].append(artist)
+    return artists
 
-# for loops 
-for car in sports_cars:
-    print(car) # will give us keys 
-    print(sports_cars[car]) # will give us values 
-
-# .keys()
-for car in sports_cars.keys():
-    print(car)
-
-# .values()
-for car in sports_cars.values():
-     print(car)
-
-# .items() - Prints both key and value. Can name the variables anything you want 
-for key,val in sports_cars.items():
-    print(key,val) 
+print(add_to_genre('country',['luke combs','kane brown','darius rucker']))
