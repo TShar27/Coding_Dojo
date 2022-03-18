623660c4a1172a6f0890250626fa1a86cb0a52ff
Timmy Shar <timshar27@gmail.com>
2022-03-17 23:00:45 -0700

    assignments

---
+++ python/weekly_work/week_2/functions_basic_ii.py
@@ -0,0 +1,13 @@
+# 1
+# Create a function that accepts a number as an input. 
+# Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
+
+def subtract_1(num):
+    for num in range(num,0,-1):
+        print(num)
+
+print(subtract_1(5))
+
+# 2
+# Create a function that will receive a list with two numbers. 
+# Print the first value and return the second.
