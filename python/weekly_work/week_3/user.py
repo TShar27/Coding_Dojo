f99237b0a092f319d45ba6bae8a179b64756a119
Timmy Shar <timshar27@gmail.com>
2022-03-26 10:07:06 -0700

    user transactions

--- python/weekly_work/week_3/user.py
+++ python/weekly_work/week_3/user.py
@@ -11,15 +11,35 @@ class User:
         self.account_balance -= amount 
     
     def display_user_balance(self):
-        print(self.name,f"(balance {self.account_balance}")
+        print(f"{self.name},Balance: ${self.account_balance}")
+    
+    def transfer_money(self,user2,amount):
+        self.account_balance -= amount
+        user2.account_balance += amount
 
 timmy = User("Timmy Shar","timmyshar@yahoo.com")
+tony = User("Tony Shar","Tonyshar5@rocketmail.com")
+olga = User("Olga Olevksy", "oolevsky@mednet.ucla.edu")
 # print(timmy.email_address)
 
 
-timmy.make_deposit(100)
-timmy.make_withdrawal(200)
-print(timmy.account_balance)
+timmy.make_deposit(700)
+olga.make_deposit(1000)
+timmy.make_deposit(250)
+timmy.make_withdrawal(100)
+tony.make_deposit(1500)
+olga.make_withdrawal(200)
+tony.make_deposit(325)
+olga.make_withdrawal(150)
+tony.make_withdrawal(200)
+
+timmy.transfer_money(olga,100)
+
+
+timmy.display_user_balance()
+tony.display_user_balance()
+olga.display_user_balance()
+# print(timmy.account_balance)
 
 
 
