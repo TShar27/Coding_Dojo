479cc9aad6b65b6fc2a60a7bc76bd0b09cc205b0
Timmy Shar <timshar27@gmail.com>
2022-05-01 13:40:41 -0700

    farm piping complete

--- python/weekly_work/week_8/ninja_gold/server.py
+++ python/weekly_work/week_8/ninja_gold/server.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request, redirect, session
+import random
 
 app = Flask(__name__)
 app.secret_key ="Timmy's a beast"
 
 @app.route('/')
 def index():
-
+    if 'gold'  not in session:
+        session['gold'] = 0
     return render_template("index.html")
+
+@app.route('/process_money',methods =['post'])
+def choice():
+    session['land'] = request.form['land']
+    random_number = random.randint(10,21)
+    if session['land'] == 'farm':
+        session['gold'] += random_number
+    print(session)
+    return redirect('/')
+
 
 @app.route('/reset')
 def clear():
