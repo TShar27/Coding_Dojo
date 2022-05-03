8dd2a924854214ad5827b8423d5af9d2e95c9074
Timmy Shar <timshar27@gmail.com>
2022-05-03 09:23:06 -0700

    initial lecture commit

---
+++ python/weekly_work/week_9/lecture/user.py
@@ -0,0 +1,8 @@
+from mysqlconnection import connectToMySQL
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
