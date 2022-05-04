f13e021ea79a46e5bec0c8f657311f954352a872
Timmy Shar <timshar27@gmail.com>
2022-05-04 09:57:37 -0700

    updated lecture code

--- python/weekly_work/week_9/lecture/server.py
+++ python/weekly_work/week_9/lecture/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, session
-
+from user import User
 app = Flask(__name__)
 app.secret_key ="kobe shar"
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    allusers = User.get_all_users()
+    return render_template("index.html",users = allusers)
 
 
 if __name__ == "__main__":
--- python/weekly_work/week_9/lecture/user.py
+++ python/weekly_work/week_9/lecture/user.py
@@ -6,3 +6,24 @@ class User:
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @classmethod
+    def get_all_users(hicls):
+        query = "SELECT * FROM users"
+        results = connectToMySQL('bankAccount').query_db(query)
+        print(results) # Dict
+        users = []
+        for user in results:
+            print(user) # each individual user line by line
+            users.append(hicls(user))
+        print(users) # array
+        return users
+
+    def get_one_user(cls):
+        pass
+    def create_user(cls,data):
+        pass
+        
