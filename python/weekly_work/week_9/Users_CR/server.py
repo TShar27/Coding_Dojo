5eebb75b07ff4439012832c9966e000096b7570b
Timmy Shar <timshar27@gmail.com>
2022-05-08 22:56:21 -0700

    more work

--- python/weekly_work/week_9/Users_CR/server.py
+++ python/weekly_work/week_9/Users_CR/server.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect, session
+from users import User
 app = Flask(__name__)
 app.secret_key ="kobe shar"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("create.html")
+
+@app.route("/create_user", methods=["post"])
+def index():
+    User.create_user(request.form)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug = True,port=5001)
