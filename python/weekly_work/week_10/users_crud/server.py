21dfa1fd3aa62dd6536bcf9267a40213888e503b
Timmy Shar <timshar27@gmail.com>
2022-05-17 09:34:08 -0700

    modularization

--- python/weekly_work/week_10/users_crud/server.py
+++ python/weekly_work/week_10/users_crud/server.py
@@ -1,39 +1,6 @@
-from flask import Flask, render_template, request, redirect, session
-from users import User
-app = Flask(__name__)
-app.secret_key ="kobe shar"
-
-@app.route("/")
-def index():
-    return render_template("index.html",users = User.get_all_users())
-
-@app.route("/read_all/create_user") # never see this URL
-def new():
-    return render_template("create.html")
-
-@app.route("/create_user", methods=["post"])
-def create_user():
-    User.create_user(request.form)
-    return redirect("/")
-
-@app.route("/show_user/<int:id>")
-def show_user(id):
-    # user = User.show_one_user(id)
-    return render_template("showOne.html", user = User.show_one_user(id))
-
-@app.route("/edit_user/<int:id>")
-def edit(id):
-    return render_template("edit.html", user = User.show_one_user(id))
-
-@app.route("/editUser/<int:id>",methods = ["post"])
-def on_page_edit(id):
-    User.update_user(request.form,id)
-    return redirect("/")
-
-@app.route("/delete/<int:id>")
-def delete_user(id):
-    User.deletion(id)
-    return redirect("/")
+from flask_app import app
+from flask_app.controllers import user_controllers
+from flask_app.models.users import User
 
 if __name__ == "__main__":
     app.run(debug = True,port=5001)
