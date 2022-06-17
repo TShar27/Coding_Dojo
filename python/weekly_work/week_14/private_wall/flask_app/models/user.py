5dc7e252bcae6acc0382402d47d2e6429949b2ed
Timmy Shar <timshar27@gmail.com>
2022-06-17 11:23:34 -0400

    login and registration page

---
+++ python/weekly_work/week_14/private_wall/flask_app/models/user.py
@@ -0,0 +1,57 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+db = 'private_wall_2'
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['first_name']) == 0:
+            is_valid = False
+            flash("Name must have an entry")
+        elif len(user['first_name']) < 3:
+            is_valid = False
+            flash("Name must be greater than 2 charachters")
+        if len(user['last_name']) == 0:
+            is_valid = False
+            flash("Name must have an entry")
+        elif len(user['last_name']) < 3:
+            is_valid = False
+            flash("Name must be greater than 2 charachters")
+        if not EMAIL_REGEX.match(user['email']):
+            is_valid = False
+            flash("Invalid Email Address.","register")
+        if len(user['password']) < 8:
+            is_valid = False
+            flash("Password must be at least 8 characters.","register")
+        if user['password'] != user['confirm']:
+            is_valid = False
+            flash("Passwords do not match!","register")
+        return is_valid
+
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO users (first_name,last_name,email,password,created_at,updated_at) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s,NOW(),NOW())"
+        return connectToMySQL(db).query_db(query,data)
+
+
+    @classmethod
+    def see_results(cls):
+        query = "SELECT * FROM users ORDER BY id DESC LIMIT 1"
+        results = connectToMySQL(db).query_db(query)
+        return User(results[0])
+
+
+        
