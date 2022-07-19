5f104e91e57e1054f5b0d2ff0e3b85b235bb9237
Timmy Shar <timshar27@gmail.com>
2022-07-18 17:36:55 -0700

    fronts initial commit

---
+++ algorithms/week_2/assignments/fronts.py
@@ -0,0 +1,4 @@
+#Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+class SLL:
+    def __init__(self,head = None):
+        self.head = head
